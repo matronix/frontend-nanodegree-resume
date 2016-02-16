6a30d4f1a8f78a24f940cfe5373da142bbdac2a9
fathemancre <ymatcheswala@gmail.com>
2016-02-16 00:37:34 -0500

    lesson 1 questions

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,28 +1,3 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
-
-/*$("#main").append("YUSUF MATCHESWALA");
-
-var awesomeThoughts = "I am YUSUF and I am awesome!!";
-
-console.log(awesomeThoughts);
-
-var funThoughts = awesomeThoughts.replace("awesome", "fun");
-
-console.log(funThoughts);
-
-$("#main").append("<p>"+funThoughts+"</p>");
-
-var formattedName;
-var formattedRole;
-
-formattedName = HTMLheaderName.replace("%data%", "Yusuf Matcheswala");
-formattedRole = HTMLheaderRole.replace("%data%", "Founder/CEO");
-
-$("#header").prepend(formattedRole);
-$("#header").prepend(formattedName);*/
-
 var bio = {
 		"name": "Yusuf M. Matcheswala",
 		"role": "Founder/CTO",
@@ -38,6 +13,8 @@ var bio = {
 		
 };
 
+
+
 formattedName = HTMLheaderName.replace("%data%", bio.name);
 formattedRole = HTMLheaderRole.replace("%data%", bio.role);
 formattedEmail = HTMLemail.replace("%data%", bio.contact.email);
@@ -51,9 +28,7 @@ formattedSkills1 = HTMLskills.replace("%data%", bio.skills[1]);
 formattedSkills2 = HTMLskills.replace("%data%", bio.skills[2]);
 
 
-/*$("#header").prepend(formattedSkills);
-$("#header").prepend(formattedMsg);
-$("#header").prepend(formattedEmail);*/
+
 $("#header").prepend(formattedMsg);
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
@@ -63,15 +38,74 @@ $("#topContacts").append(formattedEmail);
 $("#topContacts").append(formattedMobile);
 $("#topContacts").append(formattedTwitter);
 $("#topContacts").append(formattedGit);
-/*$("#topContacts").append(formattedMsg);
-$("#topContacts").append(formattedSkills);*/
+
 
 $("#header").append(HTMLskillsStart);
-$("#header").append(formattedSkills2);
-$("#header").append(formattedSkills1);
-$("#header").append(formattedSkills0);
+$("#skills").append(formattedSkills0);
+$("#skills").append(formattedSkills1);
+$("#skills").append(formattedSkills2);
 
 
+var work = {
+		"position": "CTO",
+		"name": "Labornique Inc.",
+		"years": "14"		
+};
+
+work.city = "Raleigh, NC";
+
+/*var employer = HTMLworkEmployer.replace("%data%", work.name);
+var title = HTMLworkTitle.replace("%data%", work["position"]);
+$("#workExperience").append(HTMLworkStart);
+$(".work-entry").append(employer+title);*/
+
+var projects = {
+		"title": "Connecting skilled people with those who need them",
+		"date": "Jan 2014 - Dec 2016",
+		"description": "We have mastered the technology of connecting people...specificlly connecting skilled people with those who need those skills"
+};
+
+$("#projects").append(HTMLprojectStart);
+var title = HTMLprojectTitle.replace("%data%", projects.title);
+$(".project-entry").append(title);
+var projDates = HTMLprojectDates.replace("%data%", projects.date);
+$(".project-entry").append(projDates);
+var projDescr = HTMLprojectDescription.replace("%data%", projects.description);
+$(".project-entry").append(projDescr);
+
+
+var education = {
+		"univUG": {
+			"name": "University of Mumbai, Mumbai, India",
+			"years": "4",
+			"degree": "BSEE",
+			"city": "Mumbai, India",
+			"graduated": "1997"
+		},
+
+"univPG": {
+	"name": "University of Kentucky",
+	"years": "2",
+	"degree": "MSEE",
+	"city": "Lexington, KY",
+	"graduated": "1999"
+}
+};
+
+
+
+
+var schoolName = HTMLschoolName.replace("%data%", education.univPG.name);
+schoolName = schoolName.replace("#", "http://www.uky.edu");
+var degree = HTMLschoolDegree.replace("%data%", education.univPG.degree);
+$("#education").append(HTMLschoolStart);
+$(".education-entry").append(schoolName+degree);
+var gradDate = HTMLschoolDates.replace("%data%", education.univPG.graduated);
+$(".education-entry").append(gradDate);
+var schoolLoc = HTMLschoolLocation.replace("%data%", education.univPG.city);
+$(".education-entry").append(schoolLoc);
+var major = HTMLschoolMajor.replace("%data%", education.univPG.degree);
+$(".education-entry").append(major);
 
 
 
